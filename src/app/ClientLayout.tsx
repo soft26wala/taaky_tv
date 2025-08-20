6cae1e2d6314c733453161e7f5d52ca9a8fc1a4a
Anmol452 <anmolsingh26ff@gmail.com>
2025-08-20 11:35:38 +0530

    Initial commit from Create Next App

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -4,5 +4,5 @@ import { SessionProvider } from "next-auth/react";
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   return <SessionProvider>{children}</SessionProvider>;
-}
+} 
   
